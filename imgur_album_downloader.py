2fdbe0d51d504af6e680e6192445960e395990ba
Tasos K <t.kozikopoulos@gmail.com>
2019-03-10 17:01:36 +0200

    Update imgur_album_downloader.py

--- imgur_album_downloader.py
+++ imgur_album_downloader.py
@@ -8,19 +8,20 @@ def imgur_scraper(album_id):
     i = 1
     url = 'http://imgur.com/a/{}'.format(album_id)
 
-    # Get the page's source code create the soup object.
+    # Get the page's source code and create a soup object.
     code = requests.get(url)
     text = code.text
     soup = BeautifulSoup(text, "html.parser")
-
+    
+    # Grab all the images in the gallery
     for link in soup.findAll('div', {'class': 'post-image-container'}):
         href_temp = link.get('id')
 
-        # Almost everything is .jpg, so you rarely need to change this. But if nothing is happens when you run this,
-        # then check the file extension of one of the images(they'll all be the same); they might be .png.
+        # Almost everything is .jpg, so you rarely need to change this. But if nothing happens when you run this,
+        # then check the file type of one of the images(they'll all be the same) cause they might be PNGs.
         href = 'http://i.imgur.com/' + str(href_temp) + '.jpg'
 
-        # Print the links as you download the images to make sure everything is running smoothly.
+        # Print the links as you download to make sure everything is running smoothly.
         print(href)
 
         # Save images as whatever you want, their original format doesn't really matter.
